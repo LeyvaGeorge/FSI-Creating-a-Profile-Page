355aab28af13511e3ef3a2949603b2ee129b10bd
George Leyva <leyvageorge@hotmail.com>
2022-11-16 20:27:13 -0800

    nothing done

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
+//changing header
 let content = document.querySelector('.js-generated.content')
-
 let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
 header.append('Rizzo')
 content.append(header)
 
+//
 let dogContent = document.createElement('div')
 dogContent.setAttribute('class', 'dog-content')
+
 content.append(dogContent)
 
 let dogImage = document.createElement('img')
@@ -15,8 +16,12 @@ dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
 content.append(dogImage)
 
+
+//Dog details
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
+let descript = document.createElement('h3')
+descript.textContent = 'Description'
 content.append(dogDetails)
-
+content.appand(descript)
 
